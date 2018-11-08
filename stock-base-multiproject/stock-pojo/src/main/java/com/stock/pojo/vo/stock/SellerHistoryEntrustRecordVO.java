9cbd61d4a9839b78e95a5db03d91134fee7d0c7d
cpp <cpp@example.com>
2018-11-08 12:20:20 +0800

    cpp

--- stock-base-multiproject/stock-pojo/src/main/java/com/stock/pojo/vo/stock/SellerHistoryEntrustRecordVO.java
+++ stock-base-multiproject/stock-pojo/src/main/java/com/stock/pojo/vo/stock/SellerHistoryEntrustRecordVO.java
@@ -7,10 +7,29 @@ public class SellerHistoryEntrustRecordVO extends SellerHistoryEntrustRecord {
 
 	private BaseModelVO baseModel;
 
+	private String stockCode;
+	private String stockName;
+
 	public BaseModelVO getBaseModel() {
 		return baseModel;
 	}
 	public void setBaseModel(BaseModelVO baseModel) {
 		this.baseModel = baseModel;
+	}
+
+	public String getStockCode() {
+		return stockCode;
+	}
+
+	public String getStockName() {
+		return stockName;
+	}
+
+	public void setStockCode(String stockCode) {
+		this.stockCode = stockCode;
+	}
+
+	public void setStockName(String stockName) {
+		this.stockName = stockName;
 	}
 }
